048dfee6701512b48e2c6a2a2c9b2e79f49eb252
Fraylin <Fraylinx9@gmail.com>
2026-04-16 13:00:26 -0400

    feat: tunneling support

--- server.py
+++ server.py
@@ -1178,4 +1178,4 @@ if __name__ == '__main__':
   Press Ctrl+C to stop
 """
     print(banner)
-    app.run(host='127.0.0.1', port=5000, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
